9c5b4c1fb5cebc56170177b678066fc4c81ebcf8
eakgit <eak13d@fsu.edu>
2025-01-28 12:48:02 -0500

    Removed extra files, revamped 2D edge code for proper haptic interaction and revised comments/README

--- include/MATLAB/InCircle_main.m
+++
@@ -1,2 +0,0 @@
-circle_obj = InCircle([-1 0 1],[0 1 0]);
-circle_obj.incircle_gen()
--- include/MATLAB/MPLS_main.m
+++
@@ -1,6 +0,0 @@
-%segs = {[-1,0] [0,1];[0,1] [1,0]};
-segs = {[1,1] [2,1];[2,1] [3,0];[3,0] [4,-1];[4 -1] [5 -3]};
-offset = 1;
-mpls = MPLS_Struct(offset,segs);
-%mpls.varc_gen(1,0.5)
-mpls.plotfunc()
